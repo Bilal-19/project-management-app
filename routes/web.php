6b49ff2e44918aeedf6f3ac2323fd5ec7e523d88
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-03-05 14:59:11 +0500

    Defined the routes for 'project', 'task' & 'user; controller.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -16,14 +19,20 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::redirect("/", "/dashboard");
+
+Route::middleware(['auth', 'verified'])->group(
+    function () {
+        Route::get("/dashboard", fn() => Inertia::render('Dashboard'))->name('Dashboard');
+
+        // Route::resource generates routes for 'CRUD' operations.
+        Route::resource('project', ProjectController::class);
+        Route::resource('task', TaskController::class);
+        Route::resource('user', UserController::class);
+    }
+);
+
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -35,4 +44,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
