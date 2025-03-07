9bd048bfe9e9256f914e5797dab2e5e0fe4b1547
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-03-07 12:41:06 +0500

    Style pagination.

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -6,7 +6,17 @@ export default function Pagination({ links }) {
             {
                 links.map((val) =>
                 (
-                    <Link dangerouslySetInnerHTML={{ __html: val.label }}></Link>
+                    <Link
+                        preserveScroll
+                        href={val.url || ""}
+                        key={val.label}
+                        className={
+                            "inline-block text-gray-200 text-lg px-2 py-2 rounded-lg "
+                            + (val.active ? "bg-gray-950 " : "") +
+                            (!val.url ? "!text-gray-500 cursor-not-allowed " : "hover:bg-gray-950")
+                        }
+                        dangerouslySetInnerHTML={{ __html: val.label }}
+                    ></Link>
                 )
                 )
             }
